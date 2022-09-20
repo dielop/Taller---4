2f496fb420504e8c2ccf85d95956d5ae0dba7389
Leo <leandroenardi@gmail.com>
2022-09-20 20:36:26 -0300

    Branch de Leandro para testear

--- gestion-pacientes/src/main/java/com/gestionpacientes/app/GestionPacientesApplication.java
+++ gestion-pacientes/src/main/java/com/gestionpacientes/app/GestionPacientesApplication.java
@@ -1,5 +1,5 @@
 package com.gestionpacientes.app;
-
+//Esta es la branch de Leandro
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
